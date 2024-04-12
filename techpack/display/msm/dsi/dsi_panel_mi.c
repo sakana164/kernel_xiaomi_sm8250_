383fd403c5c4172a5e242824ef95cd28d7549982
rcmiku <rcmiku@gmail.com>
2024-04-12 13:56:31 +0800

    Revert "techpack: display: Disable mi smart fps"
    This reverts commit 166aebe51346446974adda77fdfcfb2df1e3c91f.

--- techpack/display/msm/dsi/dsi_panel_mi.c
+++ techpack/display/msm/dsi/dsi_panel_mi.c
@@ -316,9 +316,10 @@ static int dsi_panel_parse_smart_fps_config(struct dsi_panel *panel,
 	struct dsi_parser_utils *utils = &panel->utils;
 	struct dsi_panel_mi_cfg *mi_cfg = &panel->mi_cfg;
 
-	mi_cfg->idle_mode_flag = false;
+	mi_cfg->idle_mode_flag = true;
 
-	mi_cfg->smart_fps_support = false;
+	mi_cfg->smart_fps_support = utils->read_bool(of_node,
+			"mi,mdss-dsi-pan-enable-smart-fps");
 
 	if (mi_cfg->smart_fps_support) {
 		pr_info("smart fps is supported\n");
