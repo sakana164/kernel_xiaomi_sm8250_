85fd95720a5236f16254a64a01b29886925cb1f9
rcmiku <rcmiku@gmail.com>
2024-11-26 14:46:53 +0800

    binder: Import Re:Kernel feature
    Signed-off-by: rcmiku <rcmiku@gmail.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -47,6 +47,10 @@
 #include <linux/capability.h>
 #include <linux/cgroup.h>
 
+#ifdef CONFIG_REKERNEL
+#include <linux/rekernel.h>
+#endif
+
 #define CREATE_TRACE_POINTS
 #include <trace/events/signal.h>
 
@@ -1269,6 +1273,14 @@ int do_send_sig_info(int sig, struct siginfo *info, struct task_struct *p,
 {
 	unsigned long flags;
 	int ret = -ESRCH;
+
+#ifdef CONFIG_REKERNEL
+	if (line_is_frozen(current) && (sig == SIGKILL || sig == SIGTERM || sig == SIGABRT || sig == SIGQUIT)) {
+			char binder_kmsg[PACKET_SIZE];
+		snprintf(binder_kmsg, sizeof(binder_kmsg), "type=Signal,signal=%d,killer_pid=%d,killer=%d,dst_pid=%d,dst=%d;", sig, task_tgid_nr(p), task_uid(p).val, task_tgid_nr(current), task_uid(current).val);
+		send_netlink_message(binder_kmsg, strlen(binder_kmsg));
+	}
+#endif
 
 	if (lock_task_sighand(p, &flags)) {
 		ret = send_signal(sig, info, p, type);
